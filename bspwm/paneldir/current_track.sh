c7878596eb9bdbc9c8492d59195282f0a20b3d7a
Han Thi Nguyen <han.nguyen15@imperial.ac.uk>
2016-04-15 15:02:04 +0200

    changed current track in panel to use mpc idle instead of constant polling

--- bspwm/paneldir/current_track.sh
+++ bspwm/paneldir/current_track.sh
@@ -1,38 +1,14 @@
 #! /bin/bash
-if [ $(pgrep -xo spotify) ]
-then
-    track=$(python2 ./.config/bspwm/paneldir/spotify.py)
-fi
 
+track=$(mpc current)
 if [[ -z $track ]]
 then
-    if [ $(pgrep -x mpd) ]
-    then
-        track=$(mpc current)
-        if [[ -z $track ]]
-        then
-            echo ""
-        elif [[ -n $(mpc status | grep paused) ]]
-        then
-            track="$track " #paused
-        else
-            track="$track " #playing
-        fi
-    else
-        echo "MPD off"
-    fi
-else
-    track="$track " #spotify
-fi
-
-# truncate tracks that are too long
-if [ ${#track} -ge 1 ]
+    echo ""
+    exit 0
+elif [[ -n $(mpc status | grep paused) ]]
 then
-    maxlen=60
-    if [ ${#track} -ge $maxlen ]
-    then
-        track=${track:${#track}-$maxlen:$maxlen}
-        track="...$track"
-    fi
-    echo $track
+    track="$track " #paused
+else
+    track="$track " #playing
 fi
+echo "$track"
---
+++ bspwm/paneldir/old_current_track.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+if [ $(pgrep -xo spotify) ]
+then
+    track=$(python2 ./.config/bspwm/paneldir/spotify.py)
+fi
+
+if [[ -z $track ]]
+then
+    if [ $(pgrep -x mpd) ]
+    then
+        track=$(mpc current)
+        if [[ -z $track ]]
+        then
+            echo ""
+            exit 0
+        elif [[ -n $(mpc status | grep paused) ]]
+        then
+            track="$track " #paused
+        else
+            track="$track " #playing
+        fi
+        echo "$track"
+    else
+        echo "MPD off"
+    fi
+else
+    track="$track " #spotify
+fi
+
+# truncate tracks that are too long
+if [ ${#track} -ge 1 ]
+then
+    maxlen=60
+    if [ ${#track} -ge $maxlen ]
+    then
+        track=${track:${#track}-$maxlen:$maxlen}
+        track="...$track"
+    fi
+    echo $track
+fi
