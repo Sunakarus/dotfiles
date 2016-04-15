8067b27c12527ad0d43f9ffcef2d00d4d5d00fab
Han Thi Nguyen <han.nguyen15@imperial.ac.uk>
2016-04-15 17:05:52 +0200

    panel cleanup

--- bspwm/paneldir/update_volume.sh
+++ bspwm/paneldir/update_volume.sh
@@ -13,7 +13,7 @@ fi
 
 #ALSA_VOLUME=$(amixer -c 1 get Master | grep 'Mono: Playback' | grep -o '...%' | sed 's/\[//' | sed 's/%//' | sed 's/ //')
 ALSA_VOLUME=$(amixer get Master | grep 'Left: Playback' | grep -o '...%' | sed 's/\[//' | sed 's/%//' | sed 's/ //')
-ALSA_STATE=$(amixer -c 1 get Master | grep 'Mono: Playback' | grep -o '\[on]')
+ALSA_STATE=$(amixer get Master | grep 'Left: Playback' | grep -o '\[on]')
 vol=""
 
 if [ "$ALSA_STATE" ]
