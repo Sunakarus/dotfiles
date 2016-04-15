d636a539fed446bfecf1b7123f9aec56b29868b8
Han Thi Nguyen <han.nguyen15@imperial.ac.uk>
2016-04-15 16:12:24 +0200

    fixed volume script

--- bspwm/paneldir/update_volume.sh
+++ bspwm/paneldir/update_volume.sh
@@ -2,18 +2,17 @@
 step=5
 if [ "$1" == "up" ]
 then
-    amixer -q sset Master $step%+
-    sleep 0.1 #delay to make sure volume changes before checking it
+    amixer set Master $step%+ -M
 elif [ "$1" == "down" ]
 then
-    amixer -q sset Master $step%-
-    sleep 0.1 #see above
+    amixer set Master $step%- -M
 elif [ "$1" == "toggle" ]
 then
     amixer set Master toggle
 fi
 
-ALSA_VOLUME=$(amixer -c 1 get Master | grep 'Mono: Playback' | grep -o '...%' | sed 's/\[//' | sed 's/%//' | sed 's/ //')
+#ALSA_VOLUME=$(amixer -c 1 get Master | grep 'Mono: Playback' | grep -o '...%' | sed 's/\[//' | sed 's/%//' | sed 's/ //')
+ALSA_VOLUME=$(amixer get Master | grep 'Left: Playback' | grep -o '...%' | sed 's/\[//' | sed 's/%//' | sed 's/ //')
 ALSA_STATE=$(amixer -c 1 get Master | grep 'Mono: Playback' | grep -o '\[on]')
 vol=""
 
