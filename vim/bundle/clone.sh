9deb6f49a6aaa5cecb8392a03c51e23abe7408c4
Han Thi Nguyen <han.nguyen15@imperial.ac.uk>
2016-10-28 10:49:55 +0100

    updated vim plugin index

--- vim/bundle/clone.sh
+++ vim/bundle/clone.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/rhysd/clever-f.vim.git
-git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/maralla/completor.vim.git
+git clone https://github.com/ctrlpvim/ctrlp.vim.git
 git clone https://github.com/Raimondi/delimitMate.git
 git clone https://github.com/morhetz/gruvbox.git
 git clone https://github.com/nanotech/jellybeans.vim.git
@@ -9,6 +10,7 @@ git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/unblevable/quick-scope.git
 git clone https://github.com/scrooloose/syntastic.git
 git clone https://github.com/godlygeek/tabular.git
+git clone https://github.com/majutsushi/tagbar
 git clone https://github.com/ternjs/tern_for_vim.git
 git clone https://github.com/coderifous/textobj-word-column.vim.git
 git clone https://github.com/vim-airline/vim-airline
@@ -17,6 +19,7 @@ git clone https://github.com/gioele/vim-autoswap.git
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/airblade/vim-gitgutter.git
 git clone https://github.com/whatyouhide/vim-gotham.git
+git clone https://github.com/ludovicchabant/vim-gutentags
 git clone https://github.com/w0ng/vim-hybrid.git
 git clone https://github.com/terryma/vim-multiple-cursors.git
 git clone https://github.com/Shougo/vimproc.vim.git
